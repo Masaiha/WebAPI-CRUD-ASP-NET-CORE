7a65883f000907aea817149ad39f4666f17d493e
Masaiha <masanori.iha@gmail.com>
2020-07-31 20:04:20 -0300

    criando API
    Controllers: Fornecedores, Produtos e Endereco

---
+++ src/DevIO.Api/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Models;
+
+namespace DevIO.Api.Extensions
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>();
+            CreateMap<Endereco, EnderecoViewModel>();
+            CreateMap<Produto, ProdutoImagemViewModel>();
+
+            CreateMap<FornecedorViewModel, Fornecedor>();
+            CreateMap<EnderecoViewModel, Endereco>();
+            CreateMap<ProdutoImagemViewModel, Produto>();
+        }
+    }
+}
